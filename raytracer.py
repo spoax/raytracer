5cdf289f773755099cc93af38347d0e79d8a7568
Serkan Kenar <serkankenar@gmail.com>
2019-04-17 23:34:36 +0400

    Chapter 2: Added Vec3 class.

--- raytracer.py
+++ raytracer.py
@@ -1,5 +1,72 @@
 # Ray Tracer in a Weekend (in Python)
-# Chapter 1 - Output an image in a window
+# Chapter 2 - The Vec3 class
+
+import math
+
+
+class Vec3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @property
+    def r(self): return self.x
+
+    @property
+    def g(self): return self.y
+
+    @property
+    def b(self): return self.z
+
+    def __neg__(self):
+        return Vec3(-self.x, -self.y, -self.z)
+
+    def __add__(self, other):
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __mul__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
+        else:
+            return Vec3(self.x * other, self.y * other, self.z * other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.x / other.x, self.y / other.y, self.z / other.z)
+        else:
+            return Vec3(self.x / other, self.y / other, self.z / other)
+
+    def __rmul__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
+        else:
+            return Vec3(self.x * other, self.y * other, self.z * other)
+
+    def __sub__(self, other):
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    @staticmethod
+    def dot(a, b):
+        return a.x * b.x + a.y * b.y + a.z * b.z
+
+    @staticmethod
+    def cross(a, b):
+        return Vec3( (a.y * b.z - a.z * b.y),
+                    -(a.x * b.z - a.z * b.x),
+                     (a.x * b.y - a.y * b.x))
+
+    @staticmethod
+    def length(a):
+        return math.sqrt(Vec3.dot(a, a))
+
+    @staticmethod
+    def squared_length(a):
+        return Vec3.dot(a, a)
+
+    @staticmethod
+    def unit_vector(v):
+        return v / Vec3.length(v)
 
 
 def make_color(r0, g0, b0):
@@ -25,12 +92,10 @@ if __name__ == '__main__':
 
     for j in range(ny):
         for i in range(nx):
-            r = float(i) / float(nx)
-            g = float(j) / float(ny)
-            b = 0.2
-            ir = int(255.99 * r)
-            ig = int(255.99 * g)
-            ib = int(255.99 * b)
+            col = Vec3(float(i) / float(nx), float(j) / float(ny), 0.2)
+            ir = int(255.99 * col.r)
+            ig = int(255.99 * col.g)
+            ib = int(255.99 * col.b)
             c = make_color(ir, ig, ib)
             img.put(c, (i, ny-j))
 
